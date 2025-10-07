7440fda078bd9ecff0df012e89ac62994f041e50
Domisnnet <dominique.goncalves61@gmail.com>
2025-10-07 15:44:25 -0300

    criação de SQL

---
+++ sql.js
@@ -0,0 +1,32 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('./banco.db');
+
+db.serialize(() => {
+    db.run(
+        `
+        CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        nome TEXT, 
+        email TEXT
+        )`
+    );
+
+    const stmt = db.prepare(
+        `
+        INSERT INTO usuarios (nome, email) VALUES (?, ?)`
+    );
+
+    stmt.run('João', 'joao@gmail.com');
+    stmt.run('Maria', 'maria@gmail.com');
+    stmt.finalize();
+
+    db.each('SELECT * FROM usuarios', (error, Linha) => {
+        if (error) throw error;
+        
+        console.log(Linha);
+        
+    });
+});
+
+db.close();
