a36c5f3489477d56a0d617e0afe6e11679e45db1
Domisnnet <dominique.goncalves61@gmail.com>
2025-10-08 10:31:37 -0300

    alteração no back end

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,25 @@
 import { PrismaClient } from './generated/prisma/index.js'; 
 
 const prisma = new PrismaClient();
+const express = require('express');
+const app = express();
+const port = 3001;
+
+// Middleware para parsear JSON no corpo das requisições
+app.use(express.json());
+
+// Middleware para CORS (Crucial para a comunicação com o Frontend)
+const cors = require('cors');
+app.use(cors()); // Configure isso depois para ser mais restritivo em produção!
+
+// Sua primeira rota de teste
+app.get('/', (req, res) => {
+  res.send('Backend rodando! ✨');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
 
 /*async function createUser() {
   const user = await prisma.usuario.create({
--- back-end/sql.js
+++ back-end/sql.js
@@ -1,4 +1,4 @@
-import sqlite3 from 'sqlite3';
+/*import sqlite3 from 'sqlite3';
 
 const db = new sqlite3.Database('./banco.db');
 
@@ -30,3 +30,4 @@ db.serialize(() => {
 });
 
 db.close();
+*/
